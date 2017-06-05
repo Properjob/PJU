8e298907bca706a871976a4e785fd6a33f175cd5
Properjob <danny-63@hotmail.co.uk>
2017-06-05 22:16:27 +0100

    Add project files.

---
+++ PJU/Functions.vb
@@ -0,0 +1,13 @@
+﻿
+Public Class Functions
+
+    Public Function EncodeURI(StringIn As String) As String
+        Return Uri.EscapeDataString(StringIn)
+    End Function
+
+    Public Function DecodeURI(StringIn As String) As String
+        Return Uri.UnescapeDataString(StringIn)
+    End Function
+
+End Class
+
---
+++ PJU/Info.vb
@@ -0,0 +1,8 @@
+﻿
+Public Class Info
+
+    Public VERSION = PJU.My.Application.Info.Version
+    Public NAME = PJU.My.Application.Info.ProductName
+
+End Class
+
