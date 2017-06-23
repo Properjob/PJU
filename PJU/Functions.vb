7c23903e1d8a04673dc87989dd4d7e32489cf40b
Properjob <danny-63@hotmail.co.uk>
2017-06-24 00:16:47 +0100

    Added ZIP File Parser

--- PJU/Functions.vb
+++ PJU/Functions.vb
@@ -9,5 +9,19 @@ Public Class Functions
         Return Uri.UnescapeDataString(StringIn)
     End Function
 
+    Public Function GetFilesFromZip(archiveFileName As String) As String()
+        Dim fileArchive As IO.Compression.ZipArchive
+        Dim Files(0) As String
+        '
+        fileArchive = IO.Compression.ZipFile.OpenRead(archiveFileName)
+        '
+        For i = 0 To fileArchive.Entries.Count - 1
+            ReDim Preserve Files(i)
+            Files(i) = fileArchive.Entries(i).Name
+        Next
+        '
+        Return Files
+    End Function
+
 End Class
 
