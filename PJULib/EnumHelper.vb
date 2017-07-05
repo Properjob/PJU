c98b41406e888f5ec0a2a3bff8f8114c46ee5d53
Properjob <danny-63@hotmail.co.uk>
2017-07-05 21:39:07 +0100

    Added PJULib

---
+++ PJULib/EnumHelper.vb
@@ -0,0 +1,19 @@
+﻿Module EnumHelper
+    Public Example As SimpleEnum
+    '
+    Dim ExampleLoaded As Boolean
+
+    Public Function ExampleItem(IndexVal) As String
+        ExampleItem = Example.Values.Item(IndexVal)
+    End Function
+
+    Sub Load()
+        Example = New SimpleEnum
+        '
+        ExampleLoad()
+    End Sub
+
+    Sub ExampleLoad()
+        Example.Add("Example1", "Just an Example")
+    End Sub
+End Module
---
+++ PJULib/SimpleEnum.vb
@@ -0,0 +1,21 @@
+﻿Public Class SimpleEnum
+    Private pValues As New Collection
+
+    ReadOnly Property Values
+        Get
+            Values = pValues
+        End Get
+    End Property
+
+
+    ReadOnly Property Count
+        Get
+            Count = pValues.Count
+        End Get
+    End Property
+
+    Public Sub Add(Key As String, Value As Object)
+        pValues.Add(Value, Key)
+    End Sub
+
+End Class
